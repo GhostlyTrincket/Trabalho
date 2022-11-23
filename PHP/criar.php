aad713e9dac6fac81ba500095b1585d71ef20fe3
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2022-11-23 14:59:10 -0300

    CSS: Mudei o espaçamento das divs por aqui. PHP: Só alterei os nomes do que esse arquivo vai pegar, para condizer com o html e não gere erros no futuro (eu espero...). OBS: Codigo do PHP foi fornecido por @alesr50

--- PHP/criar.php
+++ PHP/criar.php
@@ -1,26 +1,23 @@
 <?php
+	# codigo fornecido por @alesr50
+	$nome  = $_GET['nome_user'];
+	$email = $_GET['email_user'];
+	$senha = $_GET['senha_user'];
 
-$nome=$_GET['nome_user'];
-$email=$_GET['email_user'];
-$senha=$_GET['senha_user'];
+	// Criando conexão
+	include("./conectar.php");
+	$pdo = conectar();
 
-// Criando conexão
-include("./conectar.php");
-    $pdo = conectar();
+	$sql = "INSERT INTO usuario(nome_usuario, senha_usuario, email_usuario) VALUES (?,?,?)";
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(1, $nome);
+	$stmt->bindParam(2, $senha);
+	$stmt->bindParam(3, $email);
 
-$sql = "INSERT INTO usuario(nome_usuario, senha_usuario, email_usuario) VALUES (?,?,?,?)";
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(1, $nome);
-$stmt->bindParam(2, $login);
-$stmt->bindParam(3, $senha);
-$stmt->bindParam(4, $email);
-
-
-
-
-if ($stmt->execute()) {
-  echo "Registro Cadastrado";
-  header("Location: listar.php");
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+	if ($stmt->execute()) {
+	  echo "Registro Cadastrado";
+	  header("Location: listar.php");
+	} else {
+	  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+>
