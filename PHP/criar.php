b3423122704c2e2c6810afecdf2eb44258d2a398
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2022-12-04 16:12:00 -0300

    Adicionei o PDO para ambos os arquivos. No criar.php, eu adicionei o 'start session' para que quando o usuario criasse a sua conta, ele logar na conta de uma vez.

--- PHP/criar.php
+++ PHP/criar.php
@@ -1,26 +1,23 @@
 <?php
-	$db_servername	= "localhost";
-	$db_user	= "root";
-	$db_passwd	= "ghostlyTr1nk37";
-	$db_name	= "site";
-	
-	$DB_CONNECT = mysqli_connect($db_servername, $db_user, $db_passwd, $db_name);
-
-	if(!$DB_CONNECT) {
-		echo("Houve um erro ao conectar: ".mysqli_connect_error());
-		die();
-	}
-
 	$get_name = $_GET['nome_usuario'];
 	$get_passwd = $_GET['senha_usuario'];
 	$get_email = $_GET['email_usuario'];
 	
-	$SQL_OP = "INSERT INTO user_info(nome_usuario, senha_usuario, email_usuario)
-		VALUES('$get_name', '$get_passwd', '$get_email')";
+	include("./conectar.php");
+
+	$PDO_CONN = conn();
+	$SQL_OP = "INSERT INTO user_info(nome_usuario, senha_usuario, email_usuario) VALUES(?, ?, ?)";
 	
-	if(mysqli_query($DB_CONNECT, $SQL_OP)) {
+	$ARG = $PDO_CONN->prepare($SQL_OP);
+	$ARG->bindParam(1, $get_name);
+	$ARG->bindParam(2, $get_passwd);
+	$ARG->bindParam(3, $get_email);
+
+	if($ARG->execute()) {
 		echo("Informações registradas!");
-		header('Location: ./logar.php');
+		header('Location: a.php');
+		session_start();
+		$_SESSION['nome_usuario'] = $get_name;
 	} else {
 		echo("Houve um erro em:".$SQL_OP."<br>".mysqli_error());
 	}
--- PHP/logar.php
+++ PHP/logar.php
@@ -1,16 +1,4 @@
 <?php
-	$db_servername	= "localhost";
-	$db_user	=  "root";
-	$db_passwd	= "ghostlyTr1nk37";
-	$db_name	= "site";
-
-	$DB_CONNECT = mysqli_connect($db_servername, $db_user, $db_passwd, $db_name);
-
-	if(!$DB_CONNECT) {
-		echo("Houve um erro ao conectar: ".mysqli_connect_error());
-		die();
-	}
-
 	if(!empty($_POST) AND (empty($_POST['nome_usuario']) OR empty($_POST['senha_usuario']))) {
 		header('Location: ../HTML/logar.html');
 		exit;
@@ -19,14 +7,19 @@
 	$user = $_POST['nome_usuario'];
 	$passwd = $_POST['senha_usuario'];
 	
-	$SQL_OP = "SELECT nome_usuario, senha_usuario FROM user_info WHERE nome_usuario = '$user' AND senha_usuario = '$passwd'
-		AND ativo = 1";
-	$RESULT = mysqli_query($DB_CONNECT, $SQL_OP);
+	include("./conectar.php");
 
-	if(mysqli_num_rows($RESULT) != 1) {
+	$PDO_OP = conn();	
+	$SQL_OP = "SELECT nome_usuario, senha_usuario FROM user_info WHERE nome_usuario = ? AND senha_usuario = ? AND ativo = 1";
+
+	$ARG = $PDO_OP->prepare($SQL_OP);
+	$ARG->bindParam(1, $user);
+	$ARG->bindParam(2, $passwd);
+
+	if($ARG->execute() < 1) {
 		echo("Login invalido!");
 	} else {
-		header("Location: ../HTML/index.html");
+		header("Location: profile.php");
 		session_start();
 		$_SESSION['nome_usuario'] = $user;
 	}	
